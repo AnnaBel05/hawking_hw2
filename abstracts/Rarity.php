0430e6883a8bb7cd68302c20204e422148fd18a4
AnnaBel05 <anebuchenkova@yandex.ru>
2022-06-09 03:31:15 +0300

    hawking hw4/v2

---
+++ abstracts/Rarity.php
@@ -0,0 +1,8 @@
+<?php 
+
+abstract class Rarity
+{
+    abstract public function checkRarity(int $rarity): string;
+}
+
+?>
---
+++ interfaces/CharacterInterface.php
@@ -0,0 +1,10 @@
+<?php 
+
+namespace HW_2\interfaces;
+
+interface RarityInterface
+{
+    public function writeRarity(): string;
+}
+
+?>
